c3c8982493ca70eb81a34dcd4a15f8b47d32bd16
Clément Lucas <clement.lucas@ynotek.fr>
2016-10-03 00:27:40 +0200

    Tests object
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

---
+++ assets/tests.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const tests = [
+	{
+		image: require('./images/tests/takingCuttingsTest.jpg'),
+		titleKey: 'takingCuttingsTestTitle',
+		questionKey: 'takingCuttingsTestQuestion',
+		choices: ['takingCuttingsTestQ1', 'takingCuttingsTestQ2', 'takingCuttingsTestQ3'],
+		answer: 'takingCuttingsTestQ2',
+	},
+	{
+		image: require('./images/tests/rusticCharacterTest.jpg'),
+		titleKey: 'rusticCharacterTestTitle',
+		questionKey: 'rusticCharacterTestQuestion',
+		choices: ['rusticCharacterTestQ1', 'rusticCharacterTestQ2', 'rusticCharacterTestQ3'],
+		answer: 'rusticCharacterTestQ3',
+	},
+	{
+		image: require('./images/tests/potTest.jpg'),
+		titleKey: 'potTestTitle',
+		questionKey: 'potTestQuestion',
+		choices: ['potTestQ1', 'potTestQ2', 'potTestQ3'],
+		answer: 'potTestQ1',
+	},
+	{
+		image: require('./images/tests/waterTest.jpg'),
+		titleKey: 'takingCuttingsTestTitle',
+		questionKey: 'takingCuttingsTestQuestion',
+		choices: ['takingCuttingsTestQ1', 'takingCuttingsTestQ2', 'takingCuttingsTestQ3'],
+		answer: 'takingCuttingsTestQ2',
+	}
+];
+export default tests;
