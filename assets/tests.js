05e0ed06a5b56fad22f519108f0eafca12168d31
Clément Lucas <clement.lucas@ynotek.fr>
2016-10-03 15:46:36 +0200

    import new tests image.
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- assets/tests.js
+++ assets/tests.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 const tests = [
 	{
-		image: require('./images/tests/takingCuttingsTest.jpg'),
+		image: require('./images/tests/takingCuttingsTest.png'),
 		titleKey: 'takingCuttingsTestTitle',
 		questionKey: 'takingCuttingsTestQuestion',
 		choices: ['takingCuttingsTestQ1', 'takingCuttingsTestQ2', 'takingCuttingsTestQ3'],
 		answer: 'takingCuttingsTestQ2',
 	},
 	{
-		image: require('./images/tests/rusticCharacterTest.jpg'),
+		image: require('./images/tests/rusticCharacterTest.png'),
 		titleKey: 'rusticCharacterTestTitle',
 		questionKey: 'rusticCharacterTestQuestion',
 		choices: ['rusticCharacterTestQ1', 'rusticCharacterTestQ2', 'rusticCharacterTestQ3'],
 		answer: 'rusticCharacterTestQ3',
 	},
 	{
-		image: require('./images/tests/potTest.jpg'),
+		image: require('./images/tests/potTest.png'),
 		titleKey: 'potTestTitle',
 		questionKey: 'potTestQuestion',
 		choices: ['potTestQ1', 'potTestQ2', 'potTestQ3'],
 		answer: 'potTestQ1',
 	},
 	{
-		image: require('./images/tests/waterTest.jpg'),
+		image: require('./images/tests/waterTest.png'),
 		titleKey: 'takingCuttingsTestTitle',
 		questionKey: 'takingCuttingsTestQuestion',
 		choices: ['takingCuttingsTestQ1', 'takingCuttingsTestQ2', 'takingCuttingsTestQ3'],
