f0144e90a53f583192a58515e1f52f6fb6993e46
Clément Lucas <clement.lucas@ynotek.fr>
2016-10-07 21:55:58 +0200

    xcode: add TTF and prepare for Facebook SDK login
    Signed-off-by: Clément Lucas <clement.lucas@ynotek.fr>

--- ios/Project/AppDelegate.m
+++ ios/Project/AppDelegate.m
@@ -11,6 +11,8 @@
 
 #import "RCTRootView.h"
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -55,6 +57,20 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+//  return [[FBSDKApplicationDelegate sharedInstance] application:application
+//                                  didFinishLaunchingWithOptions:launchOptions];
+}
+
+// Facebook SDK
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                        openURL:url
+                                              sourceApplication:sourceApplication
+                                                     annotation:annotation];
 }
 
 @end
